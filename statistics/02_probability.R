67713a386209a5c6a685bfeee0e6c08fb2ea6efa
Fu-Ching Yang <fcyangesl@gmail.com>
2016-09-13 18:09:04 +0800

    Update statistics examples

--- statistics/02_probability.R
+++ statistics/02_probability.R
@@ -1,39 +1,51 @@
 library(ggplot2)
 
 ##############################################################################################
-# Sample mean/variance (LNp.1-6-82,96)
+# Sample mean/variance (LNp.1-6-82)
 ##############################################################################################
-omega <- rnorm(100000, mean=10, sd = 2)
+# Create population
+population <- rnorm(100000, mean=10, sd = 2)
 
-(sample_mean <- mean(sample( omega, 1)))
-(sample_mean <- mean(sample( omega, 5)))
-(sample_mean <- mean(sample( omega, 10)))
-(sample_mean <- mean(sample( omega, 50)))
-(sample_mean <- mean(sample( omega, 100)))
-(sample_mean <- mean(sample( omega, 1000)))
-(sample_mean <- mean(sample( omega, 10000)))
+# Create sample data from the population
+n <- 10
+observe_data <- sample( population, n)
 
-(sample_variance <- mean((sample( omega, 1) - sample_mean)^2))
-(sample_variance <- mean((sample( omega, 5) - sample_mean)^2))
-(sample_variance <- mean((sample( omega, 10) - sample_mean)^2))
-(sample_variance <- mean((sample( omega, 50) - sample_mean)^2))
-(sample_variance <- mean((sample( omega, 100) - sample_mean)^2))
-(sample_variance <- mean((sample( omega, 1000) - sample_mean)^2))
-(sample_variance <- mean((sample( omega, 10000) - sample_mean)^2))
+# Calculate sample mean
+# sample_mean = 1/n * (x1 + x2 + ... + xn)
+# R's mean() function also does the job
+(sample_mean <- sum(observe_data)/n)
+mean(observe_data)
+
+# Calculate sample variance
+# sample_var = 1/(n-1) * [ (x1-mean)^2 + (x2-mean)^2 + ... + (xn-mean)^2 ]
+# R's var() function also does the job
+(sample_var <- sum( (observe_data-sample_mean)^2 ) / (n-1))
+var(observe_data)
+
+# Note that the lower part is n-1, instead of n. This is because sample variance
+# is of n-1 degree of freedom. This is exactly what R function var() assumes.
+# Here are 2 ways to calculate variance
+sum( (observe_data-sample_mean)^2 ) / (n)
+mean((observe_data)^2) - mean(observe_data)^2
+# Here is the sample variance
+var(observe_data)
 
 ##############################################################################################
-# LLN & CLT (LNp.1-6-97)
+# LLN (LNp.1-6-95~96)
+#
+#   Let X1, X2, ..., Xn be a sequence of independent random variables with same mean and
+# variance. The mean and variance can be estimated by sample mean and sample variance. Note
+# that, those random variables are not necessary to be i.i.d.
 ##############################################################################################
-rand <- function(x) {rgamma(rnorm(rexp(x)), shape=2.5) + rbeta(rnorm(x), shape1=100, shape2=2)}
-omega <- rand(100000)
-hist(omega)
+# Create population
+population <- rnorm(100000, mean=10, sd = 2)
 
-# Plot the sample mean. A normal distribution is shown.
-sample_mean <- NULL
-for( i in c(1:1000) ) {
-    sample_mean <- c(sample_mean, mean(sample( omega, 5)))
-}
-hist(sample_mean)
+# Create sample data from the population
+n <- 50
+observe_data <- sample( population, n)
+
+mean(observe_data)
+sqrt(var(observe_data))
 
 ##############################################################################################
 # Monte Carlo integration (LNp.1-6-95)
@@ -47,6 +59,44 @@ mean(data$gx)
 
 ggplot(data, aes(x=x, y=gx)) + geom_point() + geom_bar(stat="identity") + geom_bar(stat="identity", width=0.005)
 
+##############################################################################################
+# CLT (LNp.1-6-97)
+##############################################################################################
+# Let X1, ..., Xn be i.i.d. ~ Exponential(1)
+# E(Xi) = 1
+# Var(Xi) = 1
+
+# Plot the sample mean given n=5. A normal distribution is shown.
+n <- 5
+sample_mean <- NULL
+for( i in c(1:1000) ) {
+    sample_mean <- c(sample_mean, mean(rexp(5, 1)))
+}
+hist(sample_mean)
+
+# Plot the sample mean given n=10. A normal distribution is shown.
+n <- 10
+sample_mean <- NULL
+for( i in c(1:1000) ) {
+    sample_mean <- c(sample_mean, mean(rexp(10, 1)))
+}
+hist(sample_mean)
+
+# Plot the sample mean given n=20. A normal distribution is shown.
+n <- 20
+sample_mean <- NULL
+for( i in c(1:1000) ) {
+    sample_mean <- c(sample_mean, mean(rexp(20, 1)))
+}
+hist(sample_mean)
+
+# Plot the sample mean given n=100. A normal distribution is shown.
+n <- 100
+sample_mean <- NULL
+for( i in c(1:1000) ) {
+    sample_mean <- c(sample_mean, mean(rexp(100, 1)))
+}
+hist(sample_mean)
 
 ##############################################################################################
 # Normal dist and Binomial dist approximation
--- statistics/03_point_estimation.R
+++ statistics/03_point_estimation.R
@@ -1,101 +1,75 @@
 ##############################################################################################
 # Method of finding estimators I - method of moments
-#   - Exact sampling distribution
 #
 # LNp.8-8
-# Suppose that X1, ..., Xn are i.i.d. ~ P(lambda).
-#                            _
-# The estimator of lambda is X
-# E(lambda) = lambda
-# Var(lambda) = lambda/n
-# The sampling distribution of lambda is 1/nP(n*lambda)
-# The standard error of lambda is (lambda/n)^(1/2)
+# Suppose that X1, ..., Xn are i.i.d. ~ Poisson(lambda).
+#
+# The mean of Poisson(lambda): lambda
+# The variance of Poisson(lambda): lambda
 ##############################################################################################
 x <- c(31, 29, 19, 18, 31, 28, 34, 27, 34, 30, 16, 18, 26, 27, 27, 18, 24, 22, 28, 24, 21, 17, 24)
 hist(x)
 
-# The estimator of lambda
-lambda_estimator <- function(x) { mean(x)}
+# First sample moment
+(first_sample_moment <- sum(x)/length(x))
 
-# The estimate of lambda is 24.91304
-(lambda_estimate <- lambda_estimator(x))
+# Second sample moment
+(second_sample_moment <- sum(x^2)/length(x))
 
-# The variance of lambda is 1.083176
-# (assume 24.91304 is the real lambda)
-lambda_estimate/length(x)
+# Since the lambda is the mean, whose estimator is exactly the first sample moment, so the
+# best estimator for lambda is the first sample moment.
+# Put in the observed data, the estimate is 24.91304
+(lambda_estimate <- first_sample_moment)
 
-# The standard error lambda estimator is 1.040757
-# (assume 24.91304 is the real lambda)
-(lambda_estimate/length(x))^(1/2)
+##############################################################################################
+# Estimate the variance of the estimate
+#   - Exact sampling distribution
+#   - Asymptotical method
+#   - Simulation method (bootstrap)
+#
+# Continue with the previous example.
+##############################################################################################
+# 1. Exact sampling distribution
+# By the calculation (LNp.8-10):
+#   The sampling distribution of lambda:  1/nP(n*lambda)
+#   The standard error of lambda       :  (lambda/n)^(1/2)
+sqrt((lambda_estimate/length(x))) # 1.040757
 
-# The distribution of lambda estimator
-# distribution of 1/n * P(n * lambda).
+# Plot the sampling distribution of lambda estimator
+# Note that 1/n * P(n * lambda) actually means rpois(1, 573) / 23 (LNp.8-10)
 (parameter <- length(x) * lambda_estimate)
 x=c(400:800)
 data <- data.frame(x=x, y=x/23, prob=dpois(x, 573))
 ggplot(data, aes(x=y, y=prob)) + geom_bar(stat="identity")
 
-# The sum of probability is 1
-sum(data$prob)
-
 # The probabily within 2 estimation error is 95.28%
 sum(data[data$y>24.91-2.08 & data$y<24.91+2.08,]$prob)
 
-# This is what 1/n * P(n * lambda) means in LNp.8-10
-rpois(1, 573) / 23
+# 2. Asymptotical method
+# By LLN,
+#   E(sample mean)   : population mean (LNp.1~6.95)
+#   Var(sample mean) : population variance/n (LNp.1~6.95)
+#   Var(sample mean) : sampling variance/n (LNp.1~6.96)
+(lambda_estimated <- mean(x))
+# The variance of the sample mean is variance/n
+var(x)/length(x)
+# The standard error of the sample mean is sqrt(variance/n)
+(std_error <- sqrt(var(x)/length(x))) # 1.143659
+#sqrt(lambda_estimated/length(x)) # 1.143659
 
-##############################################################################################
-# Method of finding estimators I - method of moments
-#   - Asymptotical method
-#
-# LNp.8-10
-# Suppose that X1, ..., Xn are i.i.d. ~ P(lambda).
-#
-# By CLT, sampling distribution of lambda is approximately normal when n is large enough.
-#                            _
-# The estimator of lambda is X
-# E(lambda) = lambda
-# Var(lambda) = lambda/n
-# The sampling distribution of lambda is N(lambda, lambda/n)
-# The standard error of lambda is (lambda/n)^(1/2)
-##############################################################################################
-x <- c(31, 29, 19, 18, 31, 28, 34, 27, 34, 30, 16, 18, 26, 27, 27, 18, 24, 22, 28, 24, 21, 17, 24)
+# Plot the sampling distribution of lambda estimator
+ggplot(data, aes(x=y, y=prob)) + geom_bar(stat="identity") + stat_function(fun=dnorm, args=list(mean=lambda_estimated, sd=std_error), colour="blue")
 
-# The estimator of lambda
-lambda_estimator <- function(x) { mean(x)}
-
-# The estimate of lambda is 24.91304
-(lambda_estimate <- lambda_estimator(x))
-
-# The variance of lambda is 1.083176
-# (assume 24.91304 is the real lambda)
-lambda_estimate/length(x)
-
-# The standard error lambda estimator is 1.040757
-# (assume 24.91304 is the real lambda)
-(lambda_estimate/length(x))^(1/2)
-
-# The distribution of lambda estimator
-lambda_dist2 <- function(x) {dnorm(x, 24.91304, 1.083176)}
-(parameter <- length(x) * lambda_estimate)
-x=c(400:800)
-data <- data.frame(x=x, y=x/23, prob=dpois(x, 573))
-ggplot(data, aes(x=y, y=prob)) + geom_bar(stat="identity") + stat_function(fun=lambda_dist2)
-
-# The sum of probability is 1
-integrate(lambda_dist2, 20, 30)
-
-# The probabily within 2 estimation error is 94.51%
-integrate(lambda_dist2, 24.91-2.08, 24.91+2.08)
-# According to Chebyshev's inequality: P(|X-u|>k*sd) <= 1/(k^2)
-# The probability of more than 2 standard deviation is smaller than 0.25
-# In other words, the probability within 2 standard deviation is at least 0.75
-# In this case, by fitting a normal model, same criteria says at least 0.9451 is
-# guarantee.
-1/4
-
-# This is what 1/n * P(n * lambda) means in LNp.8-10
-rpois(1, 573) / 23
+# 3. Simulation method
+# Use random number generator to generate sampling distribution.
+# The estimated parameters is from other approachs, such as moment method, MLE.
+# Here, I use the estimated parameter from moment method
+r_mean <- NULL
+for( i in c(1:1000) ) {
+    r_mean <- c(r_mean, mean(rpois(23, lambda=lambda_estimated)))
+}
+hist(r_mean)
+var(r_mean)
 
 ##############################################################################################
 # Method of finding estimators I - method of moments
@@ -108,12 +82,33 @@ rpois(1, 573) / 23
 #                            _
 # The estimator of alpha is (X^2)/(var^2)
 ##############################################################################################
-# The estimate of lambda and alpha can be obtained (via X = 0.224, variance = 0.1338)
-(lambda = 0.224 / 0.1338)
-(alpha = (0.224^2) / 0.1338)
+# First sample moment
+first_sample_moment <- 0.224
 
-# Use the estimate lambda and alpha, to generate Gamma random numbers
-# And, calculate the lambda/alpha variance
+# Second sample moment
+second_sample_moment <- 0.183976
+
+# Estimated variance
+(estimated_variance <- second_sample_moment - first_sample_moment^2)
+
+# The estimate of lambda and alpha can be obtained (via X = 0.224, variance = 0.1338)
+(lambda = first_sample_moment / estimated_variance)
+(alpha = (first_sample_moment^2) / estimated_variance)
+
+##############################################################################################
+# Estimate the variance of the estimate
+#   - Simulation method (bootstrap)
+#
+#   Exact sampling distribution is difficult to get in this example.
+#   Asymptotical method is also difficult, since involving dividend: lambda=sample_mean/variance.
+#
+# Continue with the previous example.
+##############################################################################################
+
+# 2. Simulation method
+# Use random number generator to generate sampling distribution.
+# The estimated parameters is from other approachs, such as moment method, MLE.
+# Here, I use the estimated parameter from moment method
 sim_lambda <- NULL
 sim_alpha <- NULL
 for( i in c(1:1000) ) {
@@ -127,9 +122,22 @@ for( i in c(1:1000) ) {
 # The variance of lambda is 0.12, standard deviation is 0.34
 hist(sim_lambda)
 (sim_lambda_var <- mean(sim_lambda^2 - mean(sim_lambda)^2))
-(sim_lambda_sd <- sim_lambda_var^(1/2))
+(sim_lambda_sd <- sqrt(sim_lambda_var))
 
 # The variance of alpha is 0.004, standard deviation is 0.06
 hist(sim_alpha)
 (sim_alpha_var <- mean(sim_alpha^2 - mean(sim_alpha)^2))
-(sim_alpha_sd <- sim_alpha_var^(1/2))
+(sim_alpha_sd <- sqrt(sim_alpha_var))
+
+##############################################################################################
+# Method of finding estimators II - Maximum Likelihood Estimator (MLE)
+#   - ?
+#
+# LNp.8-16
+# Suppose that X1, ..., Xn are i.i.d. ~ P(lambda).
+#                            _
+##############################################################################################
+dbinom(7, 10, 0.1)
+dbinom(7, 10, 0.5)
+dbinom(7, 10, 0.7)
+dbinom(7, 10, 0.9)
